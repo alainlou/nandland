f9826b938921e7be9597458274b1e4f8319aee3b
alainlou <az2lou@uwaterloo.ca>
2021-02-21 11:13:08 -0500

    fix if-else syntax in uart_rx

--- p7/uart_rx.v
+++ p7/uart_rx.v
@@ -1,25 +1,25 @@
 module uart_rx(
-	input FPGA_CLK,
+    input FPGA_CLK,
     input UART_RXD,
     input KEY1,
     output LED1,
-	output LED2);
+    output LED2);
 
-	reg state = 1'b1;
-	reg [25:0] counter;
+    reg state = 1'b1;
+    reg [25:0] counter;
 
-	always @(posedge FPGA_CLK, negedge UART_RXD) begin
-        if (!UART_RXD)
+    always @(posedge FPGA_CLK, negedge UART_RXD) begin
+        if (!UART_RXD) begin
+            counter <= 0;
             state <= 1'b0;
-            counter <= counter + 1;
-        else if (counter >= 50_000_000) begin
+        end else if (counter >= 50_000_000) begin
             counter <= 0;
             state <= 1'b1;
         end else
             counter <= counter + 1;
-	end
+    end
 
     assign LED1 = KEY1;
-	assign LED2 = state;
+    assign LED2 = state;
 
 endmodule
