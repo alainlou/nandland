e562e9ab2b239d47b8df53a0b2875b66d73de201
alainlou <az2lou@uwaterloo.ca>
2021-03-10 23:21:55 -0500

    add porch timings

--- p9/sync_pulse_gen.sv
+++ p9/sync_pulse_gen.sv
@@ -7,10 +7,9 @@ module sync_pulse_gen
     output reg [9:0] col
     );
 
-    parameter TOTAL_COLS  = 800;
-    parameter TOTAL_ROWS  = 525;
-    parameter ACTIVE_COLS = 640;
-    parameter ACTIVE_ROWS = 480;
+
+    parameter TOTAL_ROWS = 525, ACTIVE_ROWS = 480, V_FRONT_PORCH = 10, V_BACK_PORCH = 33;
+    parameter TOTAL_COLS = 800, ACTIVE_COLS = 640, H_FRONT_PORCH = 16, H_BACK_PORCH = 48;
 
     always @(posedge clk) begin
         if (col < TOTAL_COLS-1)
@@ -24,7 +23,7 @@ module sync_pulse_gen
         end
     end
 
-    assign HSync = col < ACTIVE_COLS;
-    assign VSync = row < ACTIVE_ROWS;
+    assign HSync = col < ACTIVE_COLS + H_FRONT_PORCH || col >= TOTAL_ROWS - H_BACK_PORCH;
+    assign VSync = row < ACTIVE_ROWS + V_FRONT_PORCH || col >= TOTAL_ROWS - V_BACK_PORCH;
 
 endmodule
